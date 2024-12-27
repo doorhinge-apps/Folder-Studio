558f39a2c4f37f619407a5de574d0b9958d270c0
Caedmon Myers <caedmonmyers@icloud.com>
2024-12-27 15:19:31 -0600

    Initial Commit

---
+++ Icons Designer/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// Icons Designer
+// ContentView.swift
+//
+// Created on 27/12/24
+//
+// Copyright ©2024 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Icons Designer/Icons_DesignerApp.swift
@@ -0,0 +1,20 @@
+//
+// Icons Designer
+// Icons_DesignerApp.swift
+//
+// Created on 27/12/24
+//
+// Copyright ©2024 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+@main
+struct Icons_DesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
